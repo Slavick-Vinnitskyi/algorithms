bf769949dd2b1dc2d5fc4e187482a910974db7b1
Slavick Vinnytskyi <andrey.belyi1@gmail.com>
2024-04-16 10:58:31 +0300

    [courses][hw4] - Add RaskinBobbins condition

---
+++ src/main/java/org/example/course_homework/hw_4/RaskinBobbins.java
@@ -0,0 +1,20 @@
+package org.example.course_homework.hw_4;
+
+/**
+ * Each time Sunny and Johnny take a trip to the Raskin Bobbins, they pool together  dollars for ice cream.
+ * On any given day, the Raskin Bobbins offers a line of  flavors.
+ * Each flavor, i, is numbered sequentially with a unique ID number from 1 to n and has a cost, Ci, associated with it.
+ * <p>
+ * Given the value of  and the cost of each flavor for  trips to the Raskin Bobbins, help Sunny and Johnny choose
+ * two flavors such that they spend their entire pool of money () during each visit.
+ * For each trip to the Raskin Bobbins, print the ID numbers for the two types of ice cream that
+ * Sunny and Johnny purchase as two space-separated integers on a new line.
+ * You must print the smaller ID first and the larger ID second.
+ * <p>
+ * Note: Two ice creams having unique IDs  and  may have the same cost (i.e.,  ).
+ */
+public class RaskinBobbins {
+    public static void main(String[] args) {
+
+    }
+}
