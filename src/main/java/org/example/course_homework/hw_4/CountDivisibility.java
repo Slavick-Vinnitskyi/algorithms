e61830f135d8ca01e978871c18c4dfef1b186cb7
Slavick Vinnytskyi <andrey.belyi1@gmail.com>
2024-04-18 16:53:08 +0300

    [courses][hw4] - Add CountDivisibility bruteforce O(n^2/2) and prefix sum solution O(n)

---
+++ src/main/java/org/example/course_homework/hw_4/CountDivisibility.java
@@ -0,0 +1,128 @@
+package org.example.course_homework.hw_4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.function.BiFunction;
+
+/**
+ * Given an array of n integers, your task is to count the number of subarrays where the sum of values is divisible by n.
+ * <p>
+ * Input Format
+ * <p>
+ * The first input line has an integer n - the size of the array.
+ * The next line has n integers: the contents of the array
+ * Print one integer: the required number of subarrays.
+ * <p>
+ * Sample Input 0
+ * <p>
+ * 5
+ * 3 1 2 7 4 8 2 8
+ * Sample Output 0
+ * <p>
+ * 1
+ */
+public class CountDivisibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+
+        for (int j = 0; j < size; j++) {
+            numbers[j] = scanner.nextInt();
+        }
+
+
+        long combinations = solveFast(numbers);
+        System.out.println(combinations);
+    }
+
+    public static long solveSlow(int[] numbers) {
+        long combination = 0;
+
+        int length = numbers.length;
+
+        for (int i = 0; i < length; i++) {
+            long sum = 0;
+            for (int j = i; j < length; j++) {
+                sum += numbers[j];
+                if (sum % length == 0) {
+                    combination++;
+                }
+            }
+        }
+        return combination;
+    }
+
+    public static int solveFast(int[] numbers) {
+        int[] prefix_sums = new int[numbers.length + 1];
+        prefix_sums[0] = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            prefix_sums[i + 1] = prefix_sums[i] + numbers[i];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>(numbers.length);
+
+
+        for (int j = 0; j < prefix_sums.length; j++) {
+            int prefix_sum = prefix_sums[j];
+            int key = prefix_sum % numbers.length;
+            add(map, key);
+        }
+
+        return map.values().stream().map(CountDivisibility::sumForCount).reduce(0, Integer::sum);
+    }
+
+    private static Integer sumForCount(Integer integer) {
+        int sum = 0;
+        for (int i = 0; i < integer; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+    private static void add(Map<Integer, Integer> map, int key) {
+        map.putIfAbsent(key, 0);
+        map.compute(key, increment());
+    }
+
+    private static BiFunction<Integer, Integer, Integer> increment() {
+        return (number, count) -> count + 1;
+    }
+
+}
+
+class CountDivisibilityBenchmark {
+    public static void main(String[] args) {
+        int count = 0;
+        while (true) {
+            int[] arr = generateArray();
+            benchmark(arr);
+            System.out.println(count++);
+        }
+    }
+
+    private static int[] generateArray() {
+        Random random = new Random();
+        int size = 200_000;
+        int[] ints = new int[size];
+        for (int i = 0; i < size; i++) {
+            ints[i] = random.nextInt(800) + 100;
+        }
+        return ints;
+    }
+
+    public static void benchmark(int[] arr) {
+        long start = System.nanoTime();
+        long combinations1 = CountDivisibility.solveSlow(arr);
+        long end1 = System.nanoTime();
+        long combinations2 = CountDivisibility.solveFast(arr);
+        long end2 = System.nanoTime();
+
+        double pow = 1_000_000_000;
+        System.out.println("fast :" + combinations1 + " time: " + (end1 - start) / pow);
+        System.out.println("slow :" + combinations2 + " time: " + (end2 - end1) / pow);
+    }
+}
