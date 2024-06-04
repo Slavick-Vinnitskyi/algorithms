27915f068a3164757bd47b9f1d24be4a0153799a
Viacheslav Vinnitskyi <v.vinnitskyi@quadient.com>
2024-06-04 18:57:11 +0300

    [courses][hw7] - Use own implementation in AdvancedStack

--- src/main/java/org/example/course_homework/hw_7/AdvancedStack.java
+++ src/main/java/org/example/course_homework/hw_7/AdvancedStack.java
@@ -3,7 +3,6 @@ package org.example.course_homework.hw_7;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +11,7 @@ import java.util.Scanner;
  */
 public class AdvancedStack {
 
-    private static final MaxStack maxStack = new MaxStack();
+    private static final Stack maxStack = new Stack();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
@@ -32,54 +31,71 @@ public class AdvancedStack {
         int operation = Integer.parseInt(s[0]);
         switch (operation) {
             case 1 -> maxStack.push(Integer.parseInt(s[1]));
-            case 2 -> maxStack.pop();
-            case 3 -> result.append(maxStack.getMaxValue() + "\n");
+            case 2 -> maxStack.poll();
+            case 3 -> result.append(maxStack.getMaxValue()).append("\n");
             default -> throw new IllegalArgumentException();
         }
     }
 
-    static class MaxStack {
-        private Integer maxValue = Integer.MIN_VALUE;
-        private final LinkedList<Pair<Integer>> list = new LinkedList<>();
+    static class Pair<T extends Integer> {
+        @Override
+        public String toString() {
+            return "el:" + element + " prop:" + property;
+        }
 
-        public MaxStack() {
+        T element;
+        T property;
+
+        public Pair(T left, T property) {
+            this.element = left;
+            this.property = property;
+        }
+    }
+
+    static class Stack {
+        Integer maxValue = Integer.MIN_VALUE;
+        Node<Pair<Integer>> head;
+        int size;
+
+        public Stack() {
+            head = null;
+            size = 0;
         }
 
         public void push(Integer value) {
             maxValue = Math.max(maxValue, value);
-            list.add(new Pair<>(value, maxValue));
+            head = new Node<>(new Pair<>(value, maxValue), head);
+            size++;
         }
 
-        public void pop() {
-            list.pollLast();
+        public Node<Pair<Integer>> poll() {
+            if (size == 0) {
+                return null;
+            }
+            var node = head;
+            head = head.next;
             maxValue = getMaxValue();
+            size--;
+            return node;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
         }
 
         public Integer getMaxValue() {
-            return list.peekLast() == null ? Integer.MIN_VALUE : list.peekLast().right;
+            return head == null ? Integer.MIN_VALUE : head.value.property;
         }
     }
 
-    static class Pair<T> {
-        @Override
-        public String toString() {
-            return "v:" + left + " max:" + right;
-        }
+    static class Node<T> {
+        T value;
+        Node<T> next;
 
-        T left;
-        T right;
-
-        public Pair(T left, T right) {
-            this.left = left;
-            this.right = right;
-        }
-
-        public T getLeft() {
-            return left;
-        }
-
-        public T getRight() {
-            return right;
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
         }
     }
+
 }
